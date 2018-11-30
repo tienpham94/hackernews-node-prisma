98d748ccf4ad155de1f343fe9a2aa868ac986a5d
Tien Pham <r3dsky02@yahoo.com.vn>
2018-11-30 21:36:17 +0200

    Move typeDefs to graphql schema

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `  
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
 
 let links = [{
   id: 'link-0',
@@ -32,7 +20,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 

148ddd102eb165dbc11eb6a3193e1dc9c63a41f3
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-02 17:00:53 +0200

    Update resolvers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
+    },
   },
 }
 

f5f831b467666d07db0e08ce1a454f4c5f141aa1
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-02 17:03:44 +0200

    Add prisma binding

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
@@ -21,7 +22,16 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/tien-pham-c8784c/howtographql/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
 
 server.start(() => console.log('server is running on port 4000'))
